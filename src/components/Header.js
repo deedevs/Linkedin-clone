0a3893840284b835e159fbec7718c3f9f998d1f4
David Ladipo <64803207+deedevs@users.noreply.github.com>
2020-12-28 14:32:35 +0200

    Templates setup and Header implementation

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>This is the header</h1>
+
+        <div className="header-left">
+            <img src="" alt=""/> 
+
+            <div className="header-search">
+                
+                <input type="text"/>
+            </div>
+
+        </div>
+
+        <div className="header-right">
+
+        </div>
+
+    </div>
+  );
+}
+
+export default Header;
