54ed1fee08df8d18b823b4b4c18f82cfa9628519
David Ladipo <64803207+deedevs@users.noreply.github.com>
2020-12-28 19:21:18 +0200

    Finished implementing Feed input

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,8 +1,36 @@
 import React from "react";
 import "./Feed.css";
 
+import CreateIcon from "@material-ui/icons/Create";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import InputOption from "./InputOption";
 function Feed() {
-  return <div></div>;
+  return (
+    <div className="feed">
+      <div className="feed-inputContainer">
+        <div className="feed-input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed-inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Feed;
