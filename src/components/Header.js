d2e83a259c6bbed2e7506106fa48cfeca137e44f
David Ladipo <64803207+deedevs@users.noreply.github.com>
2020-12-28 15:42:40 +0200

    Header Nav Implemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,36 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import HeaderOption from "./HeaderOption";
 
 function Header() {
   return (
     <div className="header">
-      <h1>This is the header</h1>
+      <div className="header-left">
+        <img
+          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+          alt="linkedin"
+        />
 
-        <div className="header-left">
-            <img src="" alt=""/> 
-
-            <div className="header-search">
-                
-                <input type="text"/>
-            </div>
-
+        <div className="header-search">
+          <SearchIcon />
+          <input type="text" placeholder="Search" />
         </div>
+      </div>
 
-        <div className="header-right">
-
-        </div>
-
+      <div className="header-right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption avatar="A" title="me" />
+      </div>
     </div>
   );
 }
---
+++ src/components/HeaderOption.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HeaderOption.css";
+import { Avatar } from "@material-ui/core";
+
+function HeaderOptions({ avatar, Icon, title }) {
+  return (
+    <div className="headerOption">
+      {Icon && <Icon className="headerOption-icon" />}
+      {avatar && <Avatar className="headerOption-avatar" src={title} />}
+      <h3 className="headerOption-title">{title}</h3>
+    </div>
+  );
+}
+
+export default HeaderOptions;
