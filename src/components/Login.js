c5ddb400e268657accc15c4e859d8c9ea9ac1633
David Ladipo <64803207+deedevs@users.noreply.github.com>
2021-01-02 17:22:10 +0200

    Login and resgiter Authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../features/userSlice";
 import { auth } from "../firebase";
 import "./Login.css";
 
@@ -7,7 +9,29 @@ function Login() {
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [profilePic, setProfilePic] = useState("");
-  const register = () => {};
+  const dispatch = useDispatch();
+  const register = () => {
+    if (!name) {
+      return alert("Please enter a full name");
+    }
+    auth.createUserWithEmailAndPassword(email, password).then((userAuth) => {
+      userAuth.user
+        .updateProfile({
+          displayName: name,
+          photoURL: profilePic,
+        })
+        .then(() => {
+          dispatch(
+            login({
+              email: userAuth.user.email,
+              uid: userAuth.user.uid,
+              displayName: name,
+              photoUrl: profilePic,
+            })
+          );
+        });
+    }).catch(error => alert(error))
+  };
 
   const loginToApp = (e) => {
     e.preventDefault();
