03be70fb0501e2112666b94c8f62ece8777a6f08
David Ladipo <64803207+deedevs@users.noreply.github.com>
2021-01-02 19:26:29 +0200

    Added react-flip-move abimation for the post feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -12,6 +12,7 @@ import { db } from "../firebase";
 import firebase from "firebase";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
+import FlipMove from "react-flip-move";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
@@ -73,15 +74,17 @@ function Feed() {
       </div>
       {/* Posts */}
 
-      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
-        <Post
-          key={id}
-          name={name}
-          description={description}
-          message={message}
-          photoUrl={photoUrl}
-        />
-      ))}
+      <FlipMove>
+        {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+          <Post
+            key={id}
+            name={name}
+            description={description}
+            message={message}
+            photoUrl={photoUrl}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
 }
