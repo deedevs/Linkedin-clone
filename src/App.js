5fb143970962a9e161e1597ccc008df99de74333
David Ladipo <64803207+deedevs@users.noreply.github.com>
2021-01-02 19:42:15 +0200

    Implementing widgets section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Feed from "./components/Feed";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Sidebar from "./components/Sidebar";
+import Widgets from "./components/Widgets";
 import { login, logout, selectUser } from "./features/userSlice";
 import { auth } from "./firebase";
 
@@ -42,7 +43,7 @@ function App() {
           <Sidebar />
           <Feed />
 
-          {/* Widgets */}
+          <Widgets /> 
         </div>
       )}
     </div>
