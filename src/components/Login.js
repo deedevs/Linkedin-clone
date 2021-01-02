ecd3b4a3dc73f45943ed6a194fbf19d7e68ba824
David Ladipo <64803207+deedevs@users.noreply.github.com>
2021-01-02 16:58:09 +0200

    Login and register form and functionaitites

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "../firebase";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const register = () => {};
+
+  const loginToApp = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
+    <div className="login">
       <img
         src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=qlR0Vks"
         alt=""
       />
 
       <form>
-          <input type="text" placeholder='Full name (required if registering)'/>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Full name (required if registering)"
+        />
+        <input
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
+          type="text"
+          placeholder="Profile pic(optional)"
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+
+        <button onClick={loginToApp}>Sign In</button>
       </form>
+      <p>
+        Not a memeber?{" "}
+        <span className="login-register" onClick={register}>
+          Register Now
+        </span>
+      </p>
     </div>
   );
 }
